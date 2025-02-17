c1ce1b78bc1c94bd1d101a2eeed75b9771767ab6
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-02-17 16:25:22 +0300

    Created 'Find Pivot Index' by A2SV Hub

---
+++ 17-Feb-2025/Find Pivot Index 269081.py
@@ -0,0 +1,14 @@
+# Problem: Find Pivot Index - https://leetcode.com/problems/find-pivot-index/
+
+class Solution:
+    def pivotIndex(self, nums):
+        total_sum = sum(nums)
+        left_sum = 0
+        
+        for i, num in enumerate(nums):
+            right_sum = total_sum - left_sum - num
+            if left_sum == right_sum:
+                return i
+            left_sum += num
+        
+        return -1
