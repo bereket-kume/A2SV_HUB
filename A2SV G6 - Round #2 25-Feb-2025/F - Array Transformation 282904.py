e2e8bd8a6e592e59cdc446c7561a43f5002472ff
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-02-26 00:08:45 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 25-Feb-2025/F - Array Transformation 282904.py
@@ -0,0 +1,26 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+
+n = int(input())
+
+for _ in range(n):
+    m = int(input())
+    nums = list(map(int, input().split()))
+
+    count = Counter(nums)
+    min_val = float('inf')
+
+    options = set(count.values())
+
+    for c in options:
+        deletion = 0
+        for num, freq in count.items():
+            if freq < c:
+                deletion += freq
+            else:
+                deletion += freq - c  
+
+        min_val = min(min_val, deletion)
+
+    print(min_val)
