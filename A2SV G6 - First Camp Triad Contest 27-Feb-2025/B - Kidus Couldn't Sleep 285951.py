2b77337fc4c6523bf3d95cd072303d218db3daee
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-02-27 15:13:46 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 27-Feb-2025/B - Kidus Couldn't Sleep 285951.py
@@ -0,0 +1,18 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+n, k = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+
+d = n - k + 1
+
+current = sum(a[:k])
+
+total = current
+
+for i in range(k, n):
+    current += a[i] - a[i-k]
+    total += current
+
+print(total/d)
