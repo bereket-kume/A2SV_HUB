ceede565ca9ea7ee707efe663a2c8a80f9184aad
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-02-27 11:20:07 +0300

    Created 'A - Kibr and His Musics' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 27-Feb-2025/A - Kibr and His Musics 285794.py
@@ -0,0 +1,18 @@
+# Problem: A - Kibr and His Musics - https://codeforces.com/gym/589822/problem/A
+
+t, m = map(int, input().split())
+prefix = []
+
+for i in range(t):
+    x, y = map(int, input().split())
+
+    prefix.append(x*y)
+    if i > 0:
+        prefix[i] += prefix[i-1]
+
+mo = list(map(int, input().split()))
+i=0
+for j in range(m):
+    while prefix[i] < mo[j]:
+        i += 1
+    print(i+1)
