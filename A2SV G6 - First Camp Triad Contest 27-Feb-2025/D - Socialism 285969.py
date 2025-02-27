d5b5c3b12f157aa8eb3e8223d05241a4460caef4
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-02-27 15:49:19 +0300

    Created 'D - Socialism' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 27-Feb-2025/D - Socialism 285969.py
@@ -0,0 +1,19 @@
+# Problem: D - Socialism - https://codeforces.com/gym/589822/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    n, x = map(int, input().split())
+
+    a = list(map(int, input().split()))
+
+    a.sort(reverse=True)
+    current_sum = 0
+    count = 0
+
+    for i in range(n):
+        current_sum += a[i]
+        if current_sum / (i+1) < x:
+            break
+        count += 1
+    print(count)
