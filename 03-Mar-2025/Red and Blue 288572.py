02d3f80b01a1dec3b21c83325d4ce6f18624d00c
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-03-03 09:56:07 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 03-Mar-2025/Red and Blue 288572.py
@@ -0,0 +1,26 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        nr = int(input())
+        r = list(map(int, input().split()))
+        
+        nl = int(input())
+        b = list(map(int, input().split()))
+
+        max_r, max_b = 0, 0
+        sum_r, sum_b = 0, 0
+
+        for num in r:
+            sum_r += num
+            max_r = max(max_r, sum_r)
+        
+        for num in b:
+            sum_b += num
+            max_b = max(max_b, sum_b)
+
+        print(max_r + max_b)
+
+if __name__ == '__main__':
+    main()
