4c277b1b9037670b55c0cea5b7e9bc758b87a54e
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-03-03 22:18:39 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 03-Mar-2025/D - Life Across the Stars 290079.py
@@ -0,0 +1,24 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+n = int(input())
+event = []
+
+for _ in range(n):
+    b, d = map(int, input().split())
+    event.append((b, 1))
+    event.append((d, -1))
+
+event.sort(key=lambda x: (x[0], x[1]))
+
+maxalive = 0
+best_year = 0
+current = 0
+
+
+for year, change in event:
+    current += change
+    if current > maxalive:
+        maxalive = current
+        best_year = year
+
+print(best_year, maxalive)
