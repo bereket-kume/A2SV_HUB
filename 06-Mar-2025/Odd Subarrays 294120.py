635d2366b63b4b95ca5a57b803123e5e05595724
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-03-06 13:49:48 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 06-Mar-2025/Odd Subarrays 294120.py
@@ -0,0 +1,20 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+
+    i, j = 0, 1
+    ans = 0
+
+    while i < n and j < n:
+        if nums[i] > nums[j]:
+            ans += 1
+            i += 2
+            j += 2
+        else:
+            i += 1
+            j += 1
+    print(ans)
