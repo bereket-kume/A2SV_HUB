2209bfdcb358c5b13b9802d3b42b83d2104a13d1
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-03-07 15:12:54 +0300

    Created 'Design Circular Deque' by A2SV Hub

---
+++ 07-Mar-2025/Design Circular Deque 295894.py
@@ -0,0 +1,63 @@
+# Problem: Design Circular Deque - https://leetcode.com/problems/design-circular-deque/
+
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.arr = deque()
+        self.k = k
+
+    def insertFront(self, value: int) -> bool:
+        if len(self.arr) >= self.k:
+            return False
+        self.arr.appendleft(value)
+        return True
+        
+    def insertLast(self, value: int) -> bool:
+        if len(self.arr) >= self.k:
+            return False
+        self.arr.append(value)
+        return True
+
+    def deleteFront(self) -> bool:
+        if not self.arr:
+            return False
+        self.arr.popleft()
+        return True
+
+    def deleteLast(self) -> bool:
+        if not self.arr:
+            return False
+        self.arr.pop()
+        return True
+        
+    def getFront(self) -> int:
+        if not self.arr:
+            return -1
+        return self.arr[0]
+
+    def getRear(self) -> int:
+        if not self.arr:
+            return -1
+        return self.arr[-1]
+
+    def isEmpty(self) -> bool:
+        if not self.arr:
+            return True
+        return False
+
+    def isFull(self) -> bool:
+        if len(self.arr) == self.k:
+            return True
+        return False
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
