790a1f6e9ed6bb42463ce0ed4869164cfaa29c8a
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-03-19 00:39:26 +0300

    Created 'A - Free Ice Cream for the Kids' by A2SV Hub

---
+++ A2SV G6 - Round #7 18-Mar-2025/A - Free Ice Cream for the Kids 311965.py
@@ -0,0 +1,17 @@
+# Problem: A - Free Ice Cream for the Kids - https://codeforces.com/gym/596141/problem/A
+
+n, x = map(int, input().split())
+count = 0
+val = x
+for _ in range(n):
+    op = input()
+    op = op.split()
+   
+    if op[0] == '+':
+        val += int(op[1])
+    else:
+        if val >= int(op[1]):
+            val -= int(op[1])
+        else:
+            count += 1
+print(val, count)
