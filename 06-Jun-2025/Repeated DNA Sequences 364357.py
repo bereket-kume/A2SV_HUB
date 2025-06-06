ef98f501491f616757bba34f67c24158e04fb7f2
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-06-06 22:22:46 +0300

    Created 'Repeated DNA Sequences' by A2SV Hub

---
+++ 06-Jun-2025/Repeated DNA Sequences 364357.py
@@ -0,0 +1,16 @@
+# Problem: Repeated DNA Sequences - https://leetcode.com/problems/repeated-dna-sequences/
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        seen = set()
+        repeated = set()
+
+        for i in range(len(s) - 9):
+            substring = s[i:i+10]
+
+            if substring in seen:
+                repeated.add(substring)
+            else:
+                seen.add(substring)
+                
+        return list(repeated)
